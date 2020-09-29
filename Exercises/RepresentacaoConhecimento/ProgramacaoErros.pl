876f6fa7356dbb3fe1efe5813df4f9ce4ca70739
PJscp16 <50209613+PJscp16@users.noreply.github.com>
2020-09-29 11:51:52 +0100

    Programação e Erros
    Representação de Conhecimento - Exercise 7

---
+++ Exercises/RepresentacaoConhecimento/ProgramacaoErros.pl
@@ -0,0 +1,4 @@
+% traduza(Codigo, Significado) -> O código de erro Codigo corresponde ao erro de significado Significado
+traduza(1, integer_overflow).
+traduza(2, divisao_por_zero).
+traduza(3, id_desconhecido).
