d04f12fb45385b162949e28b2743ed94513e7848
PJscp16 <50209613+PJscp16@users.noreply.github.com>
2020-10-13 11:25:46 +0100

    Funcionamento das Listas [H|T] 1
    Utilização de Listas - Exercise 1

---
+++ Exercises/UtilizacaoListas/FuncionamentoListasHT1.pl
@@ -0,0 +1,26 @@
+/*
+    alínea a):
+        - yes
+    alínea b):
+        - no (syntax error)
+    alínea c):
+        - H = apple, e T = [broccoli, refrigerator]
+    alínea d):
+        - H = a, e T = [b, c, d, e]
+    alínea e):
+        - H = apples, e T = [bananas]
+    alínea f):
+        - H = a, e T = [[b, c, d]]
+    alínea g):
+        - H = apples, e T = []
+    alínea h):
+        - no
+    alínea i):
+        - One = apple, Two = sprouts, e T = [fridge, milk]
+    alínea j):
+        - X = a, e Z = [Y|T]
+    alínea k):
+        - H = apple, e T = [Z]
+    alínea l):
+        - yes
+*/
