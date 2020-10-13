33fb8d4b33bb311c3efeec1bbcd8aaa0fd890f55
PJscp16 <50209613+PJscp16@users.noreply.github.com>
2020-10-13 13:44:21 +0100

    Funcionamento das Listas [H|T] 2
    Utilização de Listas - Exercise 2

---
+++ Exercises/UtilizacaoListas/FuncionamentoListasHT2.pl
@@ -0,0 +1,20 @@
+/*
+    alínea a):
+        - no
+    alínea b):
+        - C = [a], e B = [b]
+    alínea c):
+        - X = c
+    alínea d):
+        - no
+    alínea e):
+        - X = joao, Y = gosta, e Z = peixe
+    alínea f):
+        - no
+    alínea g):
+        - no
+    alínea h):
+        - Q = cavalo, e P = branco
+    alínea i):
+        - X = 1, Y = 2, Z = 3, e D = [4, 5, 6, 7]
+*/
