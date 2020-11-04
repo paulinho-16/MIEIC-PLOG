37c314103ce435384be99578c0f40c1479bf9bce
PJscp16 <50209613+PJscp16@users.noreply.github.com>
2020-11-04 10:31:03 +0000

    Efeito do Cut 2
    Unificação e Predicados CUT e NOT - Exercise 2

---
+++ Exercises/UnificacaoPredicadosCUTNOT/EfeitoCut2.pl
@@ -0,0 +1,17 @@
+p(1).
+p(2) :- !.
+p(3).
+
+/*
+    alínea a):
+        - X = 1
+        - X = 2
+    alínea b):
+        - X = 1 e Y = 1
+        - X = 1 e Y = 2
+        - X = 2 e Y = 1
+        - X = 2 e Y = 2
+    alínea c):
+        - X = 1 e Y = 1
+        - X = 1 e Y = 2
+*/
