bf4530ecea6896412602c57a0f0e0f01747fef9d
PJscp16 <50209613+PJscp16@users.noreply.github.com>
2020-11-08 12:36:03 +0000

    Definição de Operadores Diversos
    Operadores e Aritmética - Exercise 2

---
+++ Exercises/OperadoresAritmetica/DefinicaoOperadoresDiversos.pl
@@ -0,0 +1,11 @@
+:- op(500, xfx, entao).
+:- op(400, fx, se).
+:- op(400, xfx, senao).
+
+:- op(700, xfx, se).
+:- op(600, xfx, gostaria_de).
+:- op(500, xfy, e).
+:- op(400, xfx, fosse).
+
+entao(_, _).
+se(_, _).
