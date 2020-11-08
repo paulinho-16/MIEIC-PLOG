3485a9d478c64a463e4234bebb30486d3e2fbb3f
PJscp16 <50209613+PJscp16@users.noreply.github.com>
2020-11-08 15:38:40 +0000

    Definição de Operadores - Joga e E
    Operadores e Aritmética - Exercise 5

---
+++ Exercises/OperadoresAritmetica/DefinicaoOperadoresJogaE.pl
@@ -0,0 +1,11 @@
+:- op(300, xfx, joga).
+:- op(200, xfy, e).
+
+joga(_, _).
+
+/*
+    Termo T1:
+        joga(marcelo, e(futebol, squash))
+    Termo T2:
+        joga(renata, e(tenis, e(basquete, volei)))
+*/
