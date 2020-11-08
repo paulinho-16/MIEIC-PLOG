053b89cd923431f851cca8bcf355ccbe998e825f
PJscp16 <50209613+PJscp16@users.noreply.github.com>
2020-11-08 14:11:26 +0000

    Definição de Operadores para Voos
    Operadores e Aritmética - Exercise 3

---
+++ Exercises/OperadoresAritmetica/DefinicaoOperadoresVoos.pl
@@ -0,0 +1,23 @@
+/*
+    alínea a):
+                                  \\
+                             /          \
+                         //                :
+                      /     \           /     \
+                   Voo       para     Dia       :
+                          /        \         /     \
+                        de         Dest    Hora    min
+                     /      \
+                 Número    Orig
+
+    alínea b):                                          
+                                  :
+                            /           \
+                        para              :
+                      /      \       /         \
+                     de       C    para        para
+                   /    \         /    \      /    \
+                  1      A       de     C    de     B
+                                /  \        /  \
+                               2    B      3    A
+*/
