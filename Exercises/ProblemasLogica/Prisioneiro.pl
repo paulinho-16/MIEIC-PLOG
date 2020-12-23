24c1f208281007010acfb378bf21028f86c58c83
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2020-12-23 21:34:12 +0000

    O Prisioneiro
    Problemas de Lógica - Exercise 6

---
+++ Exercises/ProblemasLogica/Prisioneiro.pl
@@ -0,0 +1,8 @@
+/*
+    A pergunta a fazer é:
+        - Se eu perguntar ao seu colega qual é a porta da liberdade, qual é que ele me indicará?
+
+    Seja qual for o guarda inquirido, a resposta será a porta errada.
+    No caso de perguntarmos ao guarda que diz sempre a verdade, ele indicará a porta errada, uma vez que o guarda mentiroso indicaria a porta errada.
+    No caso de perguntarmos ao guarda mentiroso, ele indicará também a porta errada, dado que o outro guarda, que diz sempre a verdade, indicaria a porta certa.
+*/
